5b021cd3b66a9765f5fada06320a6faf604dbd32
yuriroshchanka <yuri.roshchanka@gmail.com>
2025-06-05 16:22:53 +0200

    First commit

---
+++ src/main/java/com/discount/DiscountCouponsApplication.java
@@ -0,0 +1,11 @@
+package com.discount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscountCouponsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscountCouponsApplication.class, args);
+    }
+} 
---
+++ src/main/java/com/discount/controller/Controller.java
@@ -0,0 +1,7 @@
+package com.discount.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+}
